64a25a0160242dfc5e2fc71869864e2aea829eda
AmmarBasha2011 <ammarbasha2011@gmail.com>
2025-03-13 15:47:45 +0200

    modified:   .env 	renamed:    LICENSE -> LICENSE.md 	modified:   index.php 	modified:   web/index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<script><?php echo getWEBSITEURLValue(); ?></script>
 <?php
 require_once 'functions/PHP/getPage.php';
 require_once 'functions/PHP/redirect.php';
@@ -5,5 +6,4 @@ require_once 'functions/PHP/getEnvValue.php';
 require_once 'functions/PHP/getWEBSITEURLValue.php';
 getPage($_GET['page']);
 ?>
-<script><?php echo getWEBSITEURLValue(); ?></script>
 <script><?php echo require_once 'functions/JS/redirect.js'; ?></script>
--- web/index.php
+++ web/index.php
@@ -141,7 +141,7 @@
             A High-Performance PHP Framework similar to NextJS/React, but lighter and faster. 
             Built with PHP, under 100KB, optimized for performance, and compatible with standard Apache servers.
         </p>
-        <p class="version">Version 1.0 Beta</p>
+        <p class="version">Version <?php echo getEnvValue('VERSION'); ?></p>
         <p class="edit-prompt">Let's Get Started by edit web/index.php!!!</p>
         <div class="cta-button">
             <a href="https://github.com/AmmarBasha2011/INEX-SPA">Get Started</a>
