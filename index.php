e1b6656c9f775584b9521e85ef1bdd1ca8fdbca7
AmmarBasha2011 <ammarbasha2011@gmail.com>
2025-03-13 15:49:12 +0200

    modified:   index.php

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
-<script><?php echo getWEBSITEURLValue(); ?></script>
 <?php
 require_once 'functions/PHP/getPage.php';
 require_once 'functions/PHP/redirect.php';
 require_once 'functions/PHP/getEnvValue.php';
 require_once 'functions/PHP/getWEBSITEURLValue.php';
+?>
+<script><?php echo getWEBSITEURLValue(); ?></script>
+<?php
 getPage($_GET['page']);
 ?>
 <script><?php echo require_once 'functions/JS/redirect.js'; ?></script>
